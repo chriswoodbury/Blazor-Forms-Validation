a78a3c21e54bc5ca11fc5f270ad023da66ed8e82
chriswoodbury <chris.woodbury@att.net>
2021-07-24 14:17:57 -0400

    Add project files.

---
+++ BlazorFormsValidation/Server/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorFormsValidation.Shared.Models;
+
+namespace BlazorFormsValidation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        readonly List<string> userNameList = new();
+
+        public StudentController()
+        {
+            userNameList.Add("chris");
+            userNameList.Add("janie");
+            userNameList.Add("nugget");
+        }
+
+        [HttpPost]
+        public IActionResult Post(StudentRegistration registrationData)
+        {
+            if (userNameList.Contains(registrationData.Username.ToLower()))
+            {
+                ModelState.AddModelError(nameof(registrationData.Username), "This user name is not available.");
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                return Ok(ModelState);
+            }
+
+        }
+    }
+}
---
+++ BlazorFormsValidation/Shared/Models/UserNameValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormsValidation.Shared.Models
+{
+    class UserNameValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!value.ToString().ToLower().Contains("admin"))
+            {
+                return null;
+            }
+
+            return new ValidationResult("The User Name cannot contain the word admin",
+                new[] { validationContext.MemberName });
+        }
+    }
+}
